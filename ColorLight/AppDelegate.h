5ad521b79af4eb105e7e7774d875e1c6755ed914
naoki kodama <naoki.koda@gmail.com>
2014-01-03 15:32:09 +0900

    Initial Commit

---
+++ ColorLight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorLight
+//
+//  Created by Naoki KODAMA on 2014/01/03.
+//  Copyright (c) 2014年 Naoki KODAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorLight/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorLight
+//
+//  Created by Naoki KODAMA on 2014/01/03.
+//  Copyright (c) 2014年 Naoki KODAMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
